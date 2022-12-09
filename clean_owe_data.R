50da988f5787f9b230301135fa9d4a2b4de1b8b0
Ben Zipperer <benzipperer@gmail.com>
2022-12-09 16:58:34 -0500

    remove drop down group filter from table

--- clean_owe_data.R
+++ clean_owe_data.R
@@ -58,17 +58,39 @@ write_csv(owe_database, "mw_owe_database.csv")
 
 
 # new data version
+# bibliography prep
 bib_data <- read_sheet(owe_sheet, "papers") %>% 
   select(
     study_id, matches("author_"), year, 
     title, journal, volume, number, pages, url
-  )
-
-write_csv(bib_data, "mw_owe_studies.csv")
-
+  ) %>% 
+  mutate(across(
+    matches("author_"), 
+    ~str_replace(.x, ".* ", ""), 
+    .names = "{.col}_l"
+  )) %>% 
+  mutate(
+    author = paste(author_1_l, author_2_l, author_3_l, author_4_l, author_5_l, author_6_l),
+    author = str_replace_all(author, " NA", ""),
+    author = if_else(
+      !is.na(author_1) & !is.na(author_2) & is.na(author_3) & is.na(author_4) & is.na(author_5) & is.na(author_6),
+      paste(author_1_l, "and", author_2_l),
+      author
+    ),
+    author = paste(author, year)
+  ) %>% 
+  add_count(author) %>% 
+  arrange(author, title) %>% 
+  group_by(author) %>% 
+  mutate(author = if_else(n == 2, paste0(author, letters[row_number()]), author)) %>%
+  ungroup() %>% 
+  select(-matches("_l"), -n) %>% 
+  rename(author_id = author)
+  
+# bibtex bibliography
 bib_data %>% 
   mutate(
-    author = paste(author_1, author_2, author_3, author_4, sep = ","),
+    author = paste(author_1, author_2, author_3, author_4, author_5, author_6, sep = ","),
     author = str_replace_all(author, ",NA", ""),
     author = str_split(author, ",")
   ) %>% 
@@ -76,5 +98,15 @@ bib_data %>%
   mutate(category = "ARTICLE") %>% 
   rename(CATEGORY = category, BIBTEXKEY = study_id) %>% 
   rename_with(toupper, author|year|title|journal|volume|number|pages|url) %>% 
-  df2bib("mw_owe_studies.bib")
+  df2bib("mw_owe_studies.bib")  
+
+# csv bibliography
+bib_data %>% 
+  select(-study_id) %>% 
+  relocate(author_id) %>% 
+  write_csv("mw_owe_studies.csv")
+
+owe_data <- read_sheet(owe_sheet, "estimates") %>% 
+  filter(primary == 1) %>% 
+  full_join(bib_data, by = "study_id")
   
